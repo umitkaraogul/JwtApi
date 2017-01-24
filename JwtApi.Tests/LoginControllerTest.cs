86e82f756df13f41ceb36959bebeb257e8a7c499
Ümit Karaoğul <umit.karaogull@turknet.net.tr>
2017-01-24 11:32:46 +0300

    A new test method added

--- JwtApi.Tests/LoginControllerTest.cs
+++ JwtApi.Tests/LoginControllerTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JwtApi.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace JwtApi.Tests
 {
@@ -11,6 +14,29 @@ namespace JwtApi.Tests
         public void Get()
         {
             LoginController controller = new LoginController();
+
+            string token = controller.Get("admin", "admin");
+
+            Assert.IsNotNull(token);
+        }
+        [TestMethod]
+        public void GetUsingRequest()
+        {
+            LoginController controller = new LoginController();
+            controller.Request = new HttpRequestMessage {
+                RequestUri =new Uri("http://localhost/api/products")
+            };
+            controller.Configuration = new HttpConfiguration();
+            controller.Configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+
+
+            controller.RequestContext.RouteData = new HttpRouteData(
+                route: new HttpRoute(),
+                values: new HttpRouteValueDictionary { { "controller", "login" } });
+
 
             string token = controller.Get("admin", "admin");
 
